ad650d92fe1d467da9dc478423cbeea07884e529
Sagana Ondande <sondande@oberlin.edu>
2022-01-08 14:19:31 -0500

    Inclusion of Heroku Postgres Database. Database URL in heroku account

--- music_creation.py
+++ music_creation.py
@@ -1,7 +1,15 @@
+import os
+import psycopg2
 from collections import namedtuple
 from random import choices, randint, random, randrange
 from typing import List, Callable, Tuple
 
+# Database connection to Heroku
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+# Creation of Selection Functions
 Genome = List[int]
 Population = List[Genome]
 FitnessFunc = Callable[[Genome], int]

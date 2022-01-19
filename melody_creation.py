4cf803abca9aa131d23fa3f829426547fea066c8
Sagana Ondande <sondande@oberlin.edu>
2022-01-18 20:13:50 -0500

    Implementation of Database connection/Execute command method to allow easy access to executing SQL commands from one method to minimize SQL across the application

--- melody_creation.py
+++ melody_creation.py
@@ -9,19 +9,14 @@ import os
 import psycopg2
 from random import randint, random
 
-# ARE WE CHANGING THE DATABASE INFORMATION TO INDEX OFF ZERO? OR ARE WE CHANGING THE NOTE_MAPPING ABOVE TO INDEX OFF 1?
-# I PERSONALLY LIKE INDEXING OFF ZERO, BUT WE'LL DO WHAT MAKES THE MOST SENSE TO ENSURE THE MOST SEAMLESS CAPABILITY
-# BETWEEN PYTHON AND THE DATABASE.
-#
-# Just updated all the personal id's so that they are starting off of 0. Thank you for spotting that
-# Some global variables for convenience...
-
 NOTE_MAPPING = {"C": 0, "C#": 1, "D": 2, "D#": 3, "E": 4, "F": 5, "F#": 6, "G": 7, "G#": 8, "A": 9, "A#": 10, "B": 11}
 Major = [2, 2, 1, 2, 2, 2, 1]
 Minor = [2, 1, 2, 2, 1, 2, 2]
 
 """
     Connection to database and execution of fundamental code to query through information 
+    
+    # Shows the fundamentals of connecting, utilizing, and executing commands to interact with Postgres database 
 """
 
 
@@ -33,11 +28,9 @@ def connectToDatabase():
     try:
         # Establish connection to Database if exists/ has a stable connection
         con = psycopg2.connect(DATABASE_URL)
-        #  create a new cursor
-        cur = con.cursor()
 
-        # execute an SQL statement to get the HerokuPostgres database version
-        print('PostgreSQL database version:')
+        # Create new cursor
+        cur = con.cursor()
 
         # Execute SQL command
         cur.execute("SELECT key_id, key FROM keys ORDER BY key_id")
@@ -67,8 +60,71 @@ def connectToDatabase():
             print('Database connection closed.')
 
 
-# scaleQualityAssignment: This function takes the two inputs from the main function (mainly the scale quality and
-# designates which function to send it to for interpretation.
+"""
+    Used to imported command to interact with database and return desired information
+"""
+
+
+def execute_query_command(sql_command, table):
+    # Grabs environment variable set in application configurations
+    DATABASE_URL = os.environ.get('DATABASE_URL')
+    # Set connection variable
+    con = None
+    try:
+        # Establish connection to Database if exists/ has a stable connection
+        con = psycopg2.connect(DATABASE_URL)
+
+        # Create new cursor
+        cur = con.cursor()
+
+        """
+            SQL Command Execution Section 
+        """
+        # TODO edit format of this to meet only SQL commands we want to use such as querying commands, updating, adding
+        #  might not want to include delete because we don't want to lose data and only the admins of the database
+        #  should have access to that over having that as an option in
+        # the code
+        # Checks if the command is for chromatic scale table
+        # Mainly to use just in case we are looking for a note value that could be in column note or e_harm_note
+        if table == "chromatic_scale":
+            # TODO check if the command is regarding columns note or e_harm_note
+            pass
+        else:
+            # Execute SQL command
+            cur.execute(sql_command)
+
+        # TODO Dependent on command can determine if we want to return all options or just the top one
+        # Ex: Searching for an item we would only one to return one at a time compared to seeing possible options like
+        # scales, keys, chord progressisions based off of genre input
+        # Fetch the first row if there is one, and print it out
+        db_version = cur.fetchone()
+        print("Fetching one row result:", db_version, "\n")
+
+        print("Returning all from SQL command results:")
+        return
+
+        # close the communication with the HerokuPostgres
+        cur.close()
+    except Exception as error:
+        print('Cause: {}'.format(error))
+        # Return string "Error" to prevent type error later in code when using returned value
+        return "Error"
+    # "Finally" section of try-catch statements are used to close objects and clean up resources
+    finally:
+        # close the communication with the database server by calling the close()
+        if con is not None:
+            con.close()
+            print('Database connection closed.')
+    # Included just in case of complication
+    return "Completed"
+
+
+"""
+    scaleQualityAssignment: This function takes the two inputs from the main function (mainly the scale quality and
+    designates which function to send it to for interpretation.
+"""
+
+
 def scaleQualityAssignment(root, qualityNumber):
     quality = ''
     if int(qualityNumber) == 1:
@@ -80,7 +136,11 @@ def scaleQualityAssignment(root, qualityNumber):
         minorScale(root, quality)
 
 
-# majorScale: The function that helps declare the parameters for a major scale.
+"""
+    MajorScale: The function that helps declare the parameters for a major scale
+"""
+
+
 def majorScale(root, quality):
     counter = NOTE_MAPPING[root]
     array = [NOTE_MAPPING[root]]
@@ -96,7 +156,11 @@ def majorScale(root, quality):
         print(key_list[val_list.index(array[i])], end=" ")
 
 
-# minorScale: The function that helps declare the parameters for a major scale.
+"""
+    MinorScale: The function that helps declare the parameters for a major scale
+"""
+
+
 def minorScale(root, quality):
     counter = NOTE_MAPPING[root]
     array = [NOTE_MAPPING[root]]
@@ -113,9 +177,10 @@ def minorScale(root, quality):
 
 
 # Newest additions!
-
-# crossover_function: Selects a random index from array melody_a and flips the ending halves between
-# melody_a and melody_b.
+"""
+    Crossover_function: Selects a random index from array melody_a and flips the ending halves between
+        melody_a and melody_b.
+"""
 
 def crossover_function(melody_a, melody_b):
     if len(melody_a) != len(melody_b):
@@ -125,10 +190,14 @@ def crossover_function(melody_a, melody_b):
     return melody_a[0:c_point] + melody_b[c_point:], melody_b[0:c_point] + melody_a[c_point:]
 
 
-# mutation_function: Selects two random indices from the array melody_a and flips the contents of their
-# positions. Potentially could call mutation_function multiple times to scramble up the entire chromosome.
+"""
+    Mutation_function: Selects two random indices from the array melody_a and flips the contents of their
+        positions. Potentially could call mutation_function multiple times to scramble up the entire chromosome
+"""
+
+
 # TODO we can do that actually. We would want to use it as part of the termination function because we stop the
-# algorithm when we run out of options/ met the desired amount of iterations specified
+#   algorithm when we run out of options/ met the desired amount of iterations specified
 def mutation_function(melody_a):
     print(melody_a)
 
@@ -145,7 +214,8 @@ def main():
         exit()
     elif root not in NOTE_MAPPING.keys():
         print(
-            "Not a valid root note, choose one of the following: 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'.")
+            "Not a valid root note, choose one of the following: 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', "
+            "'A', 'A#', 'B'.")
         exit()
 
     qualityNumber = input("Major or Minor? (Select 1 for major, 2 for minor): ")
